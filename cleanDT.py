cf4252a5a0c3b6077f5b2126897733d25a1cb999
Sebastian <eljhoan1411@gmail.com>
2022-10-08 13:27:13 -0500

    Init commit

---
+++ cleanDT.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+
+
+def cleanDF():
+    df = pd.read_csv('DT2-SinFronteras-SemiFinal.csv')
+
+    df.dropna(inplace = True)
+    df.to_csv('becasRequisitos3.csv', index=False)
+
+if __name__ == "__main__":
+    cleanDF()
---
+++ tf-idf-poc.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+def GetDtFromCSV():
+    df = pd.read_csv('prueba.csv')
+    return df
+
+def MakeTfIdfMatrix():
+    df = GetDtFromCSV()
+    v = TfidfVectorizer()
+    x = v.fit_transform(df['requirements'])
+    a = x.toarray()
+    s = np.shape(x)
+    print(x)
+    mat = np.matrix(a)
+    with open('outfile.txt','wb') as f:
+        for line in mat:
+            np.savetxt(f, line, fmt='%.4f')
+    
+
+if __name__ == "__main__":
+    MakeTfIdfMatrix()
