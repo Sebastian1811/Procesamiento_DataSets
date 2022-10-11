758b7460dc0b92cf0b84087d451a27e77b24cef6
Sebastian <eljhoan1411@gmail.com>
2022-10-10 23:55:09 -0500

    refactor clean empty rows script

--- cleanDT.py
+++ cleanDT.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def cleanDF():
-    df = pd.read_csv('DT2-SinFronteras-SemiFinal.csv')
+    df = pd.read_csv('Datasets_Origen/DT2-SinFronteras-SemiFinal.csv')
 
     df.dropna(inplace = True)
     df.to_csv('becasRequisitos3.csv', index=False)
