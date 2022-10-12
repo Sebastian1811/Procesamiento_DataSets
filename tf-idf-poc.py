018d29dfe2483016c369b601c6e93261f36453e1
Sebastian <eljhoan1411@gmail.com>
2022-10-11 23:44:03 -0500

    refactor tf-idf method

--- tf-idf-poc.py
+++ tf-idf-poc.py
@@ -1,22 +1,25 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+
 def GetDtFromCSV():
     df = pd.read_csv('prueba.csv')
     return df
 
-def MakeTfIdfMatrix():
-    df = GetDtFromCSV()
+def MakeTfIdfMatrix(df):
+    #df = GetDtFromCSV()
     v = TfidfVectorizer()
-    x = v.fit_transform(df['requirements'])
-    a = x.toarray()
-    s = np.shape(x)
-    print(x)
+    x = v.fit_transform(df['keywords'])
+    #a = x.toarray()
+    #s = np.shape(x)
+    """"print(x)
     mat = np.matrix(a)
     with open('outfile.txt','wb') as f:
         for line in mat:
-            np.savetxt(f, line, fmt='%.4f')
+            np.savetxt(f, line, fmt='%.4f')"""
+    return x
     
 
 if __name__ == "__main__":
-    MakeTfIdfMatrix()
+    df = GetDtFromCSV()
+    MakeTfIdfMatrix(df)
