94aca4175392b4a06bcd77d7550001d741d886fe
Sebastian Andica <eljhoan1411@gmail.com>
2022-11-03 17:48:23 -0500

    add cosine similarity model

---
+++ cos_similiraty_model.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+import numpy as np
+import pickle
+
+
+df = pd.read_csv('Datasets_procesados/DT_becas-03-11-2022-HORA-16-39-51.csv') # Dataset 
+PORCENTAJEDATASET = 0.60
+RECALCTFIDF=True
+DF_SAMPLE=[]
+
+"""
+Este metodo vectoriza la columna keywords del dataframe
+"""
+def MakeTfIdfMatrix(dataframe=df):
+    tfidf = TfidfVectorizer()
+    tfidf_matrix = tfidf.fit_transform(dataframe['keywords'])
+    #print(len(tfidf.vocabulary_))
+    return tfidf_matrix
+
+"""
+Funcion para vectorizar una muestra aleatoria del dataframe
+"""
+def VectorizeRandomSample(dataframe=df):
+    df_sample = dataframe.sample(frac=PORCENTAJEDATASET) # Extraer muestra aleatoria del dataset 60%
+    df_sample = df_sample.reset_index(drop=True)  
+    tfidf_matrix = MakeTfIdfMatrix(df_sample) # Vectorización de las palabras clave
+    with open("vectorizer.model", 'wb') as fout:
+        pickle.dump((tfidf_matrix), fout) # Guardamos el calculo de la vectorización
+    return tfidf_matrix,df_sample
+
+"""
+Funcion para cargar una matriz tfidf previamente calculada
+"""
+def LoadTfidf():
+    with open('vectorizer.model', 'rb') as f:
+        tfidf_matrix = pickle.load(f)
+    return tfidf_matrix
+
+"""
+Entrenammiento del modelo usando similaridad coseno
+"""
+def ModelTraining(tfidfMatrix):
+    cosine_sim = linear_kernel(tfidfMatrix, tfidfMatrix) # Entranamiento del modelo de recomendación
+    return cosine_sim
+"""
+Recomendar becas basado en los requisitos
+"""
+def get_recommendations(name, df,indices,cosine_sim):
+    idx = indices[name]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores,key=lambda x: x[1],reverse=True)
+    sim_scores = sim_scores[1:5]
+    print("puntajes",sim_scores)
+    beca_indices= [i[0] for i in sim_scores]
+    beca_indices = np.array(beca_indices)
+    #print("beca indices =",beca_indices)
+    return df['name'].iloc[beca_indices]
+
+if __name__ == "__main__":
+    tfidf_matrix=[]
+    if RECALCTFIDF:
+        tfidf_matrix,DF_SAMPLE = VectorizeRandomSample(df)
+    else:
+        tfidf_matrix = LoadTfidf()
+
+    model = ModelTraining(tfidf_matrix)
+   
+    indices = pd.Series(DF_SAMPLE.index, index=DF_SAMPLE['name']).drop_duplicates()
+    print("recomiendame becas parecidas a esta: ", DF_SAMPLE['name'][3])  
+    print(get_recommendations(DF_SAMPLE['name'][3],DF_SAMPLE,indices,model))
