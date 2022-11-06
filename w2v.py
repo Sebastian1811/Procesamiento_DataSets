086e44f8b7d4691efd1fdce6f7cb8b2c291d0d50
Sebastian <eljhoan1411@gmail.com>
2022-11-06 16:04:03 -0500

    refactor

--- w2v.py
+++ w2v.py
@@ -1,12 +1,9 @@
-from gensim.models import Word2Vec, FastText
+from gensim.models import Word2Vec
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import PCA
-
 from matplotlib import pyplot as plt
-
 import numpy as np
-
 import warnings
 
 
@@ -14,14 +11,22 @@ warnings.filterwarnings('ignore')
 DT_NAME = 'Datasets_procesados/DT_becas-03-11-2022-HORA-16-39-51.csv'
 df = pd.read_csv(DT_NAME)
 
-corpus = []
+def generateW2vModel():
+    """Retorna un modelo w2v usando las keywords del datset como corpus"""
+    corpus = []
+    for words in df['keywords']: 
+        corpus.append(words.split())
+    model = Word2Vec(corpus,min_count=1,vector_size=56) 
+    return model
+    
+"""corpus = []
 for words in df['keywords']: 
     corpus.append(words.split())
 
 model = Word2Vec(corpus,min_count=1,vector_size=56) 
+"""
 
-
-def vectors(x):
+def vectors(x,model):
     global word_embeddings 
     word_embeddings = []
     for line in df['keywords']:
@@ -39,10 +44,11 @@ def vectors(x):
             word_embeddings.append(avgword2vec)
 
 def recommendations(name):
-    
+    #create w2v model
+    model = generateW2vModel()
     # Calling the function vectors
 
-    vectors(df)
+    vectors(df,model)
    
     # finding cosine similarity for the vectors
 
@@ -67,6 +73,6 @@ random_beca_name = random_row['name'][0]
 
 # this example gives full precision "Becas SRE – Universidad Autónoma de Coahuila (UAdeC)"
 # another one  "Becas de Máster Universitario en Computación Gráfica, Realidad Virtual y Simulación. Fundación Repsol – Becas Fundación Carolina, 2019"
-
+# one more "Becas Erasmus +, ASTROMUNDUS – Astrophysics, 2018"
 print("recomiendame becas parecidas a esta: ",random_beca_name)
 print(recommendations(random_beca_name))
