d1e4698201156c8465c917f75578811f4bcf3f8f
Sebastian <eljhoan1411@gmail.com>
2022-11-06 22:16:49 -0500

    refactor

--- word2vec.py
+++ word2vec.py
@@ -1,11 +1,6 @@
 import pandas as pd
 import numpy as np
-import nltk
 
-
-from sklearn.metrics.pairwise import linear_kernel
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 import random
 from PIL import Image
