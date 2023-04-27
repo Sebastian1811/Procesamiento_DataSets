baed41c2a12030546b14797952b439a136225bf7
Sebastian <eljhoan1411@gmail.com>
2023-04-26 22:04:11 -0500

    add docs strings and prettier code

--- w2v.py
+++ w2v.py
@@ -2,34 +2,33 @@ from gensim.models import Word2Vec
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import euclidean_distances
-from sklearn.decomposition import PCA
 import numpy as np
 import warnings
-import plotly.graph_objects as go
 import pickle
 warnings.filterwarnings('ignore')
+
 DT_NAME = 'Datasets_procesados/DT_becas-03-11-2022-HORA-16-39-51.csv'
-RECALC_MODEL = True
+RECALC_MODEL = False
 
 df = pd.read_csv(DT_NAME)
 
 def generateW2vModel():
-    """Retorna un modelo w2v usando las keywords del datset como corpus"""
+    """Retorna un modelo w2v entrenado usando un porcentaje
+     de las keywords del datset como corpus"""
     corpus = []
     for words in df['keywords']: 
         corpus.append(words.split())
     corpus = np.array(corpus)
     corpus, corpus30 = np.split(corpus, [int(0.6 * len(corpus))])
     corpus = corpus.tolist()
-    #model = Word2Vec(corpus,min_count=1,vector_size=56) this model doesnt work
     model = Word2Vec(vector_size = 300, window=5, min_count = 2, workers = -1) 
     model.build_vocab(corpus)
     model.train(corpus, total_examples=model.corpus_count, epochs = 5)
     return model
 
-#word_embeddings = []
 def vectors(x,model):
-    """Vectoriza todas las keywords del dataframe"""
+    """Vectoriza todas las keywords de los requisitos 
+    del dataframe utilizando los scores del modelo entrenado"""
     global word_embeddings
     word_embeddings = []
     for line in df['keywords']:
@@ -48,9 +47,8 @@ def vectors(x,model):
     saveVectors(word_embeddings)
 
 def recommendations(name):
-    """Recomendar becas basado en los requisitos"""             
-    #model = generateW2vModel() #create w2v model
-    #vectors(df,model) #Generate vectors for the model
+    """Genera un listado de recomendaciones de becas basado en los vectores 
+    de los keywords de los requisitos usando medida de similitud coseno"""             
     cosine_similarities = cosine_similarity(word_embeddings, word_embeddings) # finding cosine similarity for the vectors
     indices = pd.Series(df.index, index = df['name']).drop_duplicates()
     idx = indices[name]
@@ -63,20 +61,35 @@ def recommendations(name):
     recommend = df['name'].iloc[becas_recommendations]
     return recommend 
 
+def recommendationsScores(name):
+    """Genera un listado con los puntajes de similitud de todas las becas.
+    Método desarrollado para testear el motor"""             
+    cosine_similarities = cosine_similarity(word_embeddings, word_embeddings) # finding cosine similarity for the vectors
+    indices = pd.Series(df.index, index = df['name']).drop_duplicates()
+    idx = indices[name]
+    sim_scores = list(enumerate(cosine_similarities[idx]))
+    sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse = True)
+    sim_scores = sim_scores[1:6]
+    for i in sim_scores:
+        print(i[1],',',end='')
+    print("\n")
+
 def saveVectors(vectors):
+     """Almacenar calculo de los vectores de las keywords en un archivo"""
      with open("w2v.model", 'wb') as fout:
         pickle.dump((vectors), fout) # Guardamos el calculo de la vectorización
 
 def loadVectors():
+    """Leer calculo de los vectores de las keywords en un archivo"""
     with open('w2v.model', 'rb') as f:
         w2vModel = pickle.load(f)
     return w2vModel
 
 def  rec_euc(name):
-    #model = generateW2vModel() #create w2v model
-    #vectors(df,model) #Generate vectors for the model
-
-    euclidian = euclidean_distances(word_embeddings, word_embeddings) # finding cosine similarity for the vectors
+    """Genera un listado de recomendaciones de becas
+    basado en los vectores de los requisitos usando la métrica 
+    de la distancia euclidiana"""
+    euclidian = euclidean_distances(word_embeddings, word_embeddings) # finding euclidean distance for the vectors
     indices = pd.Series(df.index, index = df['name']).drop_duplicates()
     idx = indices[name]
     sim_scores = list(enumerate(euclidian[idx])) #type: ignore
@@ -88,13 +101,17 @@ def  rec_euc(name):
     recommend = df['name'].iloc[becas_recommendations]
     return recommend 
 
-
 def getVectorForPearson(name):
+    """Método para garantizar funcionamiento de las recomendaciones
+    usando correlación de pearson"""
     indices = pd.Series(df.index, index = df['name']).drop_duplicates()
     idx = indices[name]
     return word_embeddings[idx],idx
 
 def pearsonRecommendations(myvector,idx):
+    """Genera un listado de recomendaciones de becas
+    basado en los vectores de los requisitos usando la correlación
+    de Pearson"""
     y = []
     sim_scores = []
     for i in range(len(word_embeddings)):
@@ -112,12 +129,6 @@ def pearsonRecommendations(myvector,idx):
     recommend = df['name'].iloc[becas_recommendations]
     return recommend
 
-# if RECALC_MODEL:
-#     model = generateW2vModel()
-#     vectors(df,model)
-# else:
-#     word_embeddings=loadVectors()
-
 if __name__ == "__main__":
     random_row = df.sample(n=1)
     random_row = random_row.reset_index(drop=True)  
@@ -128,13 +139,13 @@ if __name__ == "__main__":
         vectors(df,model)
     else:
         word_embeddings=loadVectors()
-    # this example gives full precision "Becas SRE – Universidad Autónoma de Coahuila (UAdeC)"
-    # another one  "Becas de Máster Universitario en Computación Gráfica, Realidad Virtual y Simulación. Fundación Repsol – Becas Fundación Carolina, 2019"
-    # one more "Becas Erasmus +, ASTROMUNDUS – Astrophysics, 2018"
-    # Becas Erasmus +, STEPS – Erasmus Mundus Master Course in Sustainable Transportation and Electrical Power Systems, 2019
+
+    # for i in df['name']:
+    #     recommendationsScores(i)
+        
     print("************************ COSINE SIMILARITY ************************************")
-    print("recomiendame becas parecidas a esta: ","Becas SRE – Universidad Autónoma de Coahuila (UAdeC)")
-    print(recommendations("Becas SRE – Universidad Autónoma de Coahuila (UAdeC)"))
+    print("recomiendame becas parecidas a esta: ",random_beca_name)
+    print(recommendations(random_beca_name))
     print("************************ EUCLIDEAN DISTANCE ************************************")
     print("recomiendame becas parecidas a esta: ",random_beca_name)
     print(rec_euc(random_beca_name))
@@ -142,4 +153,3 @@ if __name__ == "__main__":
     print("recomiendame becas parecidas a esta: ",random_beca_name)
     vector,idx = getVectorForPearson(random_beca_name)
     print(pearsonRecommendations(vector,idx))
-
